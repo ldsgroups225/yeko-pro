f75423efbd8c43b8542093658b9690a5d94504a5
Darius Kassi <kassidarius@gmail.com>
2024-12-26 20:11:41 +0000

    fix: deplyment error

--- app/t/(schools)/home/page.tsx
+++ app/t/(schools)/home/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useUserContext } from '@/providers/UserContext'
+import { useUserContext } from '@/hooks'
 import { motion } from 'framer-motion'
 import { CurrentTime, QuickAccess, SchoolInfo } from './_components'
 
